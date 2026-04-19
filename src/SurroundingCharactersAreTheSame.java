c7d4b208532ef3c2ec79c174caae16d1803a6fe7
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-19 15:21:32 +0400

    Solving task16 part5

--- src/SurroundingCharactersAreTheSame.java
+++ src/SurroundingCharactersAreTheSame.java
@@ -2,13 +2,13 @@ public class SurroundingCharactersAreTheSame {
     public static void main(String[] args){
 
         System.out.println(surroundingChar("xy*yzz"));
-
+        System.out.println();
     }
     public static boolean surroundingChar(String str){
 
 
         for(int i = 0; i < str.length(); i++){
-            if (str.charAt(i) == "*"){
+            if (str.charAt(i) == '*'){
                 if (str.charAt(i - 1) == str.charAt(i + 1)){
                     return true;
                 }
