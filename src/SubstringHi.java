02f4c5788f130fef018d823848d093c66efa15e9
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-19 10:29:02 +0400

    Solving Task2 Part1

---
+++ src/SubstringHi.java
@@ -0,0 +1,11 @@
+public class SubstringHi {
+    public static void main{
+
+    }
+
+    public static String hiAppears (String str){
+        int count = 0;
+
+        for(int i = 0; i <)
+    }
+}
