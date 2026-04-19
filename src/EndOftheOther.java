63a569176134844f219a97dce7b20c78a83e8819
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-19 11:11:58 +0400

    Solving Task5 part1

---
+++ src/EndOftheOther.java
@@ -0,0 +1,14 @@
+public class EndOftheOther {
+    public static void main(String[] args){
+
+
+    }
+    public static boolean endOfTheOther (String str1 ,String str2){
+
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+
+        if (str1.length() >= str)
+
+    }
+}
