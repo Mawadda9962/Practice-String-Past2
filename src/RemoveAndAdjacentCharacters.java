c7b0544567d3bfdf4f1431cdebf64956b47087af
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-19 16:28:09 +0400

    Solving task19 Part3

--- src/RemoveAndAdjacentCharacters.java
+++ src/RemoveAndAdjacentCharacters.java
@@ -1,15 +1,20 @@
+import java.beans.PropertyEditorSupport;
+
 public class RemoveAndAdjacentCharacters {
-    public static void main (String[] args {
+    public static void main (String[] args){
 
     }
     public static String removeChar (String str){
 
+        String result = "";
         for(int i = 0; i < str.length(); i++){
             if(str.charAt(i) == '*'){
                 i++;
+                continue;
 
             }
-            if(str.charAt(i -1)  )
         }
+        return result;
+
     }
 }
