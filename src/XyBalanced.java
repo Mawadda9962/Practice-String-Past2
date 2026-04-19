a9236aeeff0e9aabb0f839c7edf186559f8fa513
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-19 12:08:00 +0400

    Solving Task8 xy-balanced

--- src/XyBalanced.java
+++ src/XyBalanced.java
@@ -2,12 +2,11 @@ public class XyBalanced {
     public static void main(String[] args) {
 
         System.out.println(xyBalanced("aaxbby"));
-        System.out.println("aaxbb");
     }
 
     public static boolean xyBalanced(String str) {
         for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == 'x' && str.charAt(i + 1) == 'y') {
+            if (str.charAt(i) == 'x' && str.charAt(i + 3) == 'y') {
 
                 return true;
             }
