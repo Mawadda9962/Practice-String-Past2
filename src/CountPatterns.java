d6c622c6a6bd721b1a70034eda1adf3479230b18
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-19 11:04:42 +0400

    Solving Task4 Count patterns

--- src/CountPatterns.java
+++ src/CountPatterns.java
@@ -1,7 +1,7 @@
 public class CountPatterns {
     public static void main(String[] args){
         System.out.println(countPatterns("aaacodebbb"));
-        System.out.println(countPatterns);
+        System.out.println(countPatterns("codexxcode"));
 
 
     }
@@ -9,7 +9,7 @@ public class CountPatterns {
 
         int count = 0;
         for(int i = 0; i < str.length() -1; i++){
-            if(str.substring(i, i + 1).equals("co")){
+            if(str.substring(i, i + 2).equals("co")){
                 count ++;
 
             }
