426718faa23efba8ae239517167e649d2a65946c
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-19 12:00:37 +0400

    Solving Task8 Part4

--- src/XyBalanced.java
+++ src/XyBalanced.java
@@ -1,11 +1,15 @@
 public class XyBalanced {
     public static void main(String[] args) {
 
+        System.out.println(xyBalanced("aaxbby"));
+        System.out.println("aaxbb");
     }
 
     public static boolean xyBalanced(String str) {
         for (int i = 0; i < str.length(); i++) {
             if (str.substring(i).equals("x")) {
+
+            }
                 if (str.substring(str.length()).equals("y")) {
                     return true;
                 }
