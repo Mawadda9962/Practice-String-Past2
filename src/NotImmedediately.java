93d0201975ba24a1ffc2e36459c2c57320e1c068
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-19 11:36:48 +0400

    solving Task6 part2

--- src/NotImmedediately.java
+++ src/NotImmedediately.java
@@ -1,13 +1,18 @@
 public class NotImmedediately {
     public static void main(String[] args){
 
+        System.out.println(notImmediately("abcxyz"));
+        System.out.println(notImmediately("abc.xyz"));
     }
-    public static boolean notImmediately(String str){
-        for(int i = 0; i < str.length() -1; i++){
-            if (str.substring(i ,i + 3).equals("xyz"));
+    public static boolean notImmediately(String str) {
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.substring(i, i + 3).equals("xyz")) {
+
+            if (i == 0 || str.charAt(i - 1) != '.') {
+                return true;
+            }
         }
-           if (i == 0 || str.charAt(i - 1) != '.' ){
-               return true;
-           }
+    }
+    return false;
     }
 }
