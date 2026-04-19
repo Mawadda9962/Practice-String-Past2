ed80846a12756dda73f04bb77fec2319e106c755
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-19 14:08:27 +0400

    Solving Task12 Part2

--- src/RepeatWithSeparator.java
+++ src/RepeatWithSeparator.java
@@ -5,11 +5,10 @@ public class RepeatWithSeparator {
     }
     public static String repeatWord (String str, String Sep, int n){
 
-        String last = str.substring(str.length() - n);
         String result = "";
 
         for(int i = 0; i < n; i++){
-            result = result + last;
+            result += str ;
         }
 
         return result;
