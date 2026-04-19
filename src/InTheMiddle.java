20030cf3fadd6022b8092b8af0c1f8959012dd07
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-19 14:45:02 +0400

    Solving task14 xyz in the middle

--- src/InTheMiddle.java
+++ src/InTheMiddle.java
@@ -3,17 +3,25 @@ public class InTheMiddle {
 
         System.out.println(xyzInTheMiddle("AAxyzBB"));
         System.out.println(xyzInTheMiddle("AxyzBB"));
-        System.out.println();
+        System.out.println(xyzInTheMiddle("AxyzBBB"));
     }
-    public static boolean xyzInTheMiddle (String str) {
-        for (int i = 0; i < str.length(); i++) {
-            if (str.substring(i, i+3).equals("xyz")) {
+    public static boolean xyzInTheMiddle(String str){
 
-                return true;
+        int len = str.length();
+
+        for(int i = 0; i <= len - 3; i++){
+
+            if(str.substring(i, i + 3).equals("xyz")){
+
+                int left = i;
+                int right = len - (i + 3);
+
+                if(Math.abs(left - right) <= 1){
+                    return true;
+                }
             }
-
         }
-        return false;
 
+        return false;
     }
 }
