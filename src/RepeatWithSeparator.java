bd2b4ffd9f20e4afbf920a2e8771745858114b92
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-19 14:11:25 +0400

    Solving Task12 Repeat word with separator

--- src/RepeatWithSeparator.java
+++ src/RepeatWithSeparator.java
@@ -1,14 +1,14 @@
 public class RepeatWithSeparator {
     public static void main(String[] args){
 
-
+        System.out.println(repeatWord("Word", "X", 3));
     }
     public static String repeatWord (String str, String Sep, int n){
 
         String result = "";
 
         for(int i = 0; i < n; i++){
-            result += str ;
+            result += str + Sep ;
         }
 
         return result;
