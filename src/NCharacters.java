8d1579f12ff87a14fa6abc7acbec7e7c6acb8269
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-19 12:27:26 +0400

    Solving Task10 part2

--- src/NCharacters.java
+++ src/NCharacters.java
@@ -2,7 +2,15 @@ public class NCharacters {
     public static void main(String[] args){
 
     }
-    public static String nCharacters (String n){
+    public static String repeatEnd(String str, int n){
 
+        String last = str.substring(str.length() - n);
+        String result = "";
+
+        for(int i = 0; i < n; i++){
+            result = result + last;
+        }
+
+        return result;
     }
 }
