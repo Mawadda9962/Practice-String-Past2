2de8293b96a9d2d969760d3c3c4a9f899376af73
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-19 16:26:34 +0400

    Solving task19 Part2

--- src/RemoveAndAdjacentCharacters.java
+++ src/RemoveAndAdjacentCharacters.java
@@ -4,5 +4,12 @@ public class RemoveAndAdjacentCharacters {
     }
     public static String removeChar (String str){
 
+        for(int i = 0; i < str.length(); i++){
+            if(str.charAt(i) == '*'){
+                i++;
+
+            }
+            if(str.charAt(i -1)  )
+        }
     }
 }
