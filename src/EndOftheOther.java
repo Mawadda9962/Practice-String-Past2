ab5668b8ce3b63bc253589e947435b1e91a2d76d
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-19 11:14:53 +0400

    Solving part4

--- src/EndOftheOther.java
+++ src/EndOftheOther.java
@@ -1,7 +1,7 @@
 public class EndOftheOther {
     public static void main(String[] args){
 
-
+        System.out.println(endOfTheOther());
     }
     public static boolean endOfTheOther (String str1 ,String str2){
 
