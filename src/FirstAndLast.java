cc72e4fa9acb7d3c8afe7e95916332bea5bc1871
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-19 14:52:26 +0400

    Solving task15 part3

--- src/FirstAndLast.java
+++ src/FirstAndLast.java
@@ -1,13 +1,14 @@
 public class FirstAndLast {
     public static void main(String[] args){
 
+        System.out.println(firstAndLast("breadjambread"));
     }
     public static String firstAndLast (String str){
 
         int first = str.indexOf("bread");
         int last = str.lastIndexOf("bread");
 
-        return str.substring(first +5,last);
+        return str.substring(first + 5,last);
 
     }
 }
