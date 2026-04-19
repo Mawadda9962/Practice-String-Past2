4e91b4803c066ec9db372748ad0200a9ef6c7ead
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-19 17:09:35 +0400

    Solving task21 part1

--- src/ReturnChar.java
+++ src/ReturnChar.java
@@ -1,8 +1,30 @@
 public class ReturnChar {
     public static void main(String[] args){
 
-    }
-    public static String returnBeforeAndAfter(String str,){
+        System.out.println(returnBeforeAndAfter("abcXY123XYijk", "XY"));
+        System.out.println(returnBeforeAndAfter("XY1XY", "XY"));
 
+    }
+
+    public static String returnBeforeAndAfter(String str, String word){
+
+        String result = "";
+        int lenWord = word.length();
+
+        for(int i = 0; i <= str.length() - lenWord; i++){
+
+            if(str.substring(i, i + lenWord).equals(word)){
+
+                if(i > 0){
+                    result += str.charAt(i - 1);
+                }
+
+                if(i + lenWord < str.length()){
+                    result += str.charAt(i + lenWord);
+                }
+            }
+        }
+
+        return result;
     }
 }
