6b8f392fb1773920e453d71f31874eb30983b7e6
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-19 10:58:58 +0400

    Solving Task4 part 2

--- src/CountPatterns.java
+++ src/CountPatterns.java
@@ -5,6 +5,14 @@ public class CountPatterns {
     }
     public static Integer countPatterns (String str){
 
+        int count = 0;
+        for(int i = 0; i < str.length() -1; i++){
+            if(str.substring(i, i + 1).equals("co")){
+                count ++;
+
+            }
+        }
+        return count;
 
     }
 }
