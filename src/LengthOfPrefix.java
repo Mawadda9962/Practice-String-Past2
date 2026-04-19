2f09cded922a653a4545b0154f338c83216479b7
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-19 14:29:31 +0400

    Solving Task13 prefix of length N

--- src/LengthOfPrefix.java
+++ src/LengthOfPrefix.java
@@ -1,10 +1,11 @@
 public class LengthOfPrefix {
     public static void main(String[] args){
 
+        System.out.println(prefixOfLength("abXYabc", 1));
 
     }
     public static Boolean prefixOfLength(String str, int n){
-        return(str.substring(0, n) = str.substring(n));
+        return(str.substring(n).contains(str.substring(0, n)));
 
     }
 }
