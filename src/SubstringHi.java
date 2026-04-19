547ce0f91fd4bac62472c7856d32df6dc1e3a228
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-19 10:35:05 +0400

    solving part 3

--- src/SubstringHi.java
+++ src/SubstringHi.java
@@ -8,8 +8,8 @@ public class SubstringHi {
     public static String hiAppears(String str){
         int count = 0;
 
-        for(int i = 0; i < str.length(); i++){
-            if (str.length(i) == "hi"){
+        for(int i = 0; i < str.length() -1; i++){
+            if (str.substring(i, i + 2).equals("hi")){
                 count ++;
             }
         }
