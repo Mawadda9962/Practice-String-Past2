60caf635bf8d52e9b635c08c274519b74ac6fb8e
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-19 14:36:31 +0400

    Solving Task14 part4

--- src/InTheMiddle.java
+++ src/InTheMiddle.java
@@ -1,8 +1,8 @@
 public class InTheMiddle {
     public static void main(String[] args){
 
-
         System.out.println(xyzInTheMiddle("AAxyzBB"));
+        System.out.println();
     }
     public static boolean xyzInTheMiddle (String str) {
         for (int i = 0; i < str.length(); i++) {
