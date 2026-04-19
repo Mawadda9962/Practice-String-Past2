bb709a0411861c64fbb0b6dd72927f438c1de555
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-19 10:23:06 +0400

    Solving part 4

--- src/CharRepetedTwice.java
+++ src/CharRepetedTwice.java
@@ -2,7 +2,7 @@ public class CharRepetedTwice {
     public static void main(String[] args){
 
         String message = "The";
-        System.out.println(message);
+        System.out.println(doubleChar(message));
     }
 
     public static String doubleChar (String mass){
