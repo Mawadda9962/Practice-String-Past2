d7b4baa15320e4d9194e50223e7a55c447e1b548
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-19 17:11:18 +0400

    Solving task21 Return characters before and after each occurrence of a word

--- src/ReturnChar.java
+++ src/ReturnChar.java
@@ -2,7 +2,6 @@ public class ReturnChar {
     public static void main(String[] args){
 
         System.out.println(returnBeforeAndAfter("abcXY123XYijk", "XY"));
-
     }
 
     public static String returnBeforeAndAfter(String str, String word){
