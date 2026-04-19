73e22fcd98198e2b3bb42892b9f8c8dd814f49cf
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-19 11:50:41 +0400

    solving part 2

--- src/bPattern.java
+++ src/bPattern.java
@@ -4,6 +4,16 @@ public class bPattern {
     }
     public static boolean BPattern (String str){
 
+        for(int i = 0; i < str.length(); i++){
+            if(str.substring(i).equals("b")){
+
+            }
+            if(str.substring(i + 2).equals("b")){
+            }
+            return true;
+        }
+        return false;
+
     }
 }
 
