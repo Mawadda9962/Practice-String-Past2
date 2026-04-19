55767c4c942a81e58ceadb75aff00af6453b75cd
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-19 16:54:37 +0400

    Solving task20 Replace all characters with '+' except given word

--- src/ReplacingAllChar.java
+++ src/ReplacingAllChar.java
@@ -1,20 +1,27 @@
 public class ReplacingAllChar {
     public static void main(String[] args) {
 
-
+        System.out.println(replacingAllChar("12xy34"));
     }
 
     public static String replacingAllChar(String str) {
 
+        String result = "";
+
         for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == 'x' && str.charAt(i + 1) == 'y') {
-                System.out.println(str.charAt(i));
+
+            if (i < str.length() - 1 &&
+                    str.charAt(i) == 'x' &&
+                    str.charAt(i + 1) == 'y') {
+
+                result += "xy";
+                i++;
 
             } else {
-                System.out.println("+");
+                result += "+";
             }
-
-
         }
+
+        return result;
     }
 }
