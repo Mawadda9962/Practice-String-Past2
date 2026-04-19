43fae73fa27bed1077b0dafc0846ba7165ef4365
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-19 16:45:46 +0400

    Solving task20 part2

--- src/ReplacingAllChar.java
+++ src/ReplacingAllChar.java
@@ -1,9 +1,20 @@
 public class ReplacingAllChar {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
 
     }
-    public static String replacingAllChar(String str){
 
+    public static String replacingAllChar(String str) {
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'x' && str.charAt(i + 1) == 'y') {
+                System.out.println(str.charAt(i));
+
+            } else {
+                System.out.println("+");
+            }
+
+
+        }
     }
 }
