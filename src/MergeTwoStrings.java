6937dbe7212aac0f42946937ffda2e67b17102ee
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-19 12:24:01 +0400

    Solving Task9 Merge two strings

--- src/MergeTwoStrings.java
+++ src/MergeTwoStrings.java
@@ -1,6 +1,8 @@
 public class MergeTwoStrings {
     public static void main(String[] args){
 
+        System.out.println(mergeTwoStrings("abc", "xyz"));
+
 
     }
     public static String mergeTwoStrings (String str1 ,String str2 ){
@@ -9,7 +11,7 @@ public class MergeTwoStrings {
         int  i =0;
 
         while(i < str1.length() && i < str2.length()){
-            result += str1.charAt(i) + str2.length(i);
+            result += str1.charAt(i) + str2.charAt(i);
             i++;
         }
         result += str1.substring(i) + str2.substring(i);
