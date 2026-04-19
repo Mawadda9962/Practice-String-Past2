13dde87804fc4daa35e078c3750b845fbb406bf7
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-19 15:45:58 +0400

    Solving task18 part1

---
+++ src/RemoveMiddleCharacter.java
@@ -0,0 +1,10 @@
+public class RemoveMiddleCharacter {
+    public static void main(String[] args){
+
+
+    }
+    public static String removeMidd (String str){
+
+        String result = "";
+    }
+}
