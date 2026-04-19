3e77cef6ce7773343f5b0c022282ec942655ea30
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-19 14:36:12 +0400

    Solving Task14 part3

--- src/InTheMiddle.java
+++ src/InTheMiddle.java
@@ -2,11 +2,11 @@ public class InTheMiddle {
     public static void main(String[] args){
 
 
-        System.out.println();
+        System.out.println(xyzInTheMiddle("AAxyzBB"));
     }
     public static boolean xyzInTheMiddle (String str) {
         for (int i = 0; i < str.length(); i++) {
-            if (str.substring(i).equals("xyz")) {
+            if (str.substring(i, i+3).equals("xyz")) {
 
                 return true;
             }
