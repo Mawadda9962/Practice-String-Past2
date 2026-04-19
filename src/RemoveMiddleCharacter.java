9759978b6ad79967feb7d43ecf969f382dc9acb9
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-19 15:53:11 +0400

    Solving task18 part2

--- src/RemoveMiddleCharacter.java
+++ src/RemoveMiddleCharacter.java
@@ -6,5 +6,15 @@ public class RemoveMiddleCharacter {
     public static String removeMidd (String str){
 
         String result = "";
+
+        for(int i = 0; i < str.length(); i++){
+
+            if (i < str.length() -2 && str.charAt(i) =='z' && str.charAt(i + 2) == 'p'){
+
+            }
+            result += "z";
+            result += "p";
+            i += 2;
+        }
     }
 }
