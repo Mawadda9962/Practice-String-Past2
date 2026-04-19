7a8d3dbeec9b460c482ea0d52af5b2b964d60dfe
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-19 15:41:48 +0400

    Solving task17 Fix oneTwo logic

--- src/MoveTheFirstToEnd.java
+++ src/MoveTheFirstToEnd.java
@@ -1,15 +1,15 @@
 public class MoveTheFirstToEnd {
     public static void main(String[] args){
-        System.out.println(firstToEnd());
+        System.out.println(firstToEnd("abc"));
 
     }
-    public static Boolean firstToEnd(String str){
+    public static String firstToEnd(String str){
 
         String result = "";
 
-        for(int i = 0; i < str.length(); i++){
+        for(int i = 0; i < str.length(); i += 3){
 
-            result += str.charAt(i + 3);
+            result += str.charAt(i + 1);
             result += str.charAt(i + 2);
             result += str.charAt(i);
 
