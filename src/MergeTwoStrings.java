3a3ef0d3d04c69fb93fc281d57224b241aadeb87
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-19 12:20:59 +0400

    Solving Task9 Part2

--- src/MergeTwoStrings.java
+++ src/MergeTwoStrings.java
@@ -5,6 +5,15 @@ public class MergeTwoStrings {
     }
     public static String mergeTwoStrings (String str1 ,String str2 ){
 
+        String result = "";
+        int  i =0;
+
+        while(i < str1.length() && i < str2.length()){
+            result += str1.charAt(i) + str2.length(i);
+            i++;
+        }
+        result += str1.substring(i) +
+
 
 
     }
